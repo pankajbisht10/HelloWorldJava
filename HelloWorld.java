259f03d1701b40416a6620a6fac3df6bd1ee9fad
Pankaj Bisht <pankajbisht10@yahoo.com>
2017-06-29 12:20:19 +0530

    Changed the print message for Jenkins

--- HelloWorld.java
+++ HelloWorld.java
@@ -4,7 +4,7 @@ public class HelloWorld {
 
              for(int i=1;i<=10;i++){
 
-                     System.out.println("Hello World ...."+i);
+                     System.out.println("Hello World from Jenkins...."+i);
                 }
 
         }
